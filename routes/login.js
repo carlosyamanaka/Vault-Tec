cea8b86ffca891a62f4a3bb01119f211de8fa687
Akio Suzuki <kiosuzuki1@gmail.com>
2024-05-19 12:56:11 -0300

    Implement session and authentication
    - Added session management using express-session
    - Configured environment variables for admin
    - Created login route

---
+++ routes/login.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const router = express.Router()
+
+const Usuario = require('../models/Usuario')
+
+login = function(req, res, next) {
+    let {email, senha} = req.body
+    let usuario = Usuario.getByLogin(email, senha)
+    if (usuario == null) {
+        req.session.messages = ["Falha ao realizar o login."]
+        res.redirect("/?login=1")   
+    } else {
+        req.session.user = usuario
+        res.redirect("/intranet?login=1")
+    }
+}
+
+let salvaUsuario = function(req, res, next) {
+    if (req.body.lembrar) {
+        res.cookie("email", req.body.email, {maxAge: 7 * 24 * 60 * 60 * 1000})
+    } else {
+        res.clearCookie("email")
+    }
+    return next()
+}
+
+let logout = function(req, res, next) {
+    req.session.user = null;
+    res.redirect("/")
+}
+
+router.post("/login", salvaUsuario, login)
+router.get("/logout", logout)
+
+module.exports = router
---
+++ routes/main.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const router = express.Router()
+
+const Acesso = require('../helpers/acesso')
+const Eventos = require('../models/Evento')
+const Usuario = require('../models/Usuario')
+
+let portfolio = [];
+
+router.get("/", (req, res) => {
+    let error = ""
+    if (req.session.messages != undefined) {
+        error = req.session.messages.pop()
+    }
+    res.render("index", {
+        eventos: Eventos.publicos(), 
+        email: req.cookies.email,
+        error: error,
+        portfolio: portfolio})
+})
+
+router.get("/intranet", Acesso.estaLogado, (req, res) => {
+    res.render("restrito", {eventos: Eventos.privados(), 
+                            usuario: req.session.user.nome, 
+                            isAdmin: Usuario.isAdmin(req.session.user),
+                            portfolio: portfolio})
+})
+
+router.post("/criarperfil", Acesso.estaLogado, (req, res) => {
+    const { nome, url } = req.body;
+    const novoPerfil = { nome, url };
+    console.log('Criando perfil')
+    // Adiciona o novo perfil à lista de perfis
+    portfolio.push(novoPerfil);
+    res.redirect('/intranet?login=1');
+});
+
+router.get("/perfil/:url", (req, res) => {
+    const { url } = req.params;
+    const perfil = portfolio.find(perfil => perfil.url === url);
+    if (!perfil) {
+        return res.status(404).send('Perfil não encontrado');
+    }
+    res.render('perfil', { nome: perfil.nome, url });
+});
+
+router.get("/admin", Acesso.ehAdmin, (req, res) => {
+    res.render("admin", {eventos: Eventos.todos(), usuario: "Admin"})
+})
+
+router.get("/loginautomatico", Acesso.login)
+
+module.exports = router
